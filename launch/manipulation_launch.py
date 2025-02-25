84df150a7dd867a1f66c818f3eab28c277d3f3a0
oberton31 <bertonoliver50@gmail.com>
2025-02-24 21:58:47 -0500

    creating first launch file

---
+++ launch/manipulation_launch.py
@@ -0,0 +1,20 @@
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='snaak_pneumatic',
+            executable='snaak_pneumatic_control.py',
+            name='pneumatic_control'),
+
+        launch_ros.actions.Node(
+            package='snaak_manipulation',
+            executable='manipulation_actions_node.py',
+            name='manipulation_action_server'),
+
+        launch_ros.actions.Node(
+            package='snaak_manipulation',
+            executable='manipulations_primary_node.py',
+            name='manipulation_primary_node')
+    ])
